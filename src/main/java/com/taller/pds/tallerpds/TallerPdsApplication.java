f2de102d9cd826f8c642908f16892a7784ed969c
Edward Andres Moron Quinntana <edwaramor9@gmail.com>
2022-01-18 10:54:08 -0500

    initial commit

---
+++ src/main/java/com/taller/pds/tallerpds/TallerPdsApplication.java
@@ -0,0 +1,13 @@
+package com.taller.pds.tallerpds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerPdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerPdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taller/pds/tallerpds/TallerPdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.pds.tallerpds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerPdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
