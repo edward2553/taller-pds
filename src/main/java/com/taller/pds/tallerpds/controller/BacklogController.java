ef686dd39d0f7947332cedcee4cc2d121e339836
Edward Andres Moron Quinntana <edwaramor9@gmail.com>
2022-01-31 12:46:40 -0500

    Se terminan las validaciones de en la capa de las entidades

--- src/main/java/com/taller/pds/tallerpds/controller/BacklogController.java
+++ src/main/java/com/taller/pds/tallerpds/controller/BacklogController.java
@@ -1,25 +1,39 @@
 package com.taller.pds.tallerpds.controller;
 
 import com.taller.pds.tallerpds.entities.Backlog;
+import com.taller.pds.tallerpds.helper.Helpers;
+import com.taller.pds.tallerpds.helper.ResponseBuilder;
+import com.taller.pds.tallerpds.model.Response;
 import com.taller.pds.tallerpds.services.BacklogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/backlog")
+@RequiredArgsConstructor
 public class BacklogController {
 
-    @Autowired
-    private BacklogService service;
+    private final BacklogService service;
+    private final ResponseBuilder builder;
+    private final Helpers helpers;
 
     @GetMapping
     public List<Backlog> findAll() { return service.findAll(); }
 
     @PostMapping
-    public Backlog create(@RequestBody Backlog backlog){
-        return service.create(backlog);
+    public Response create(@Valid @RequestBody Backlog backlog, BindingResult validationResult){
+        if (validationResult.hasErrors()) {
+            return builder.failed(helpers.formatMessage(validationResult));
+        }
+        service.create(backlog);
+        return builder.success(backlog);
     }
+
+
 
 }
